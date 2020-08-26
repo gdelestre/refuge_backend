307e8ea6a61328cb2acb72408e75a847de88b761
ghislain.delestre <delestre.ghislain@gmail.com>
2020-08-26 18:12:14 +0200

    mise à jour des fichiers à compléter

--- src/test/java/utilities/ConnexionDB_a_compléter.java
+++ src/test/java/utilities/ConnexionDB_a_compléter.java
@@ -1,4 +1,23 @@
 package utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnexionDB_a_compléter {
+    public static Connection give_connection() {
+        Connection myConnex = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/refuge?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+            String username = "a compléter";
+            String password = "a compléter";
+
+            myConnex = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection is created successfully");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Connection failed");
+        }
+        return myConnex;
+    }
+
 }
